d1ecfbbda8043033ebf3702c5c17fa381df13738
Angela Clarke <angeladoust@hotmail.com>
2022-06-25 11:26:45 +1000

    created project models

--- crowdfunding/projects/models.py
+++ crowdfunding/projects/models.py
@@ -1,3 +1,14 @@
+from turtle import title
+from unittest.util import _MAX_LENGTH
 from django.db import models
 
 # Create your models here.
+
+class Project(models.Model):
+    title = models.CharField(max_length=500)
+    description = models.TextField()
+    goal = models.IntegerField()
+    image = models.URLField()
+    is_open = models.BooleanField()
+    date_created = models.DateTimeField()
+    owner = models.CharField(max_length=200)
