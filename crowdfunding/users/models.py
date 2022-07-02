9fce37fdecc2389f007336e19401fa10d9aa4cc3
Angela Clarke <angeladoust@hotmail.com>
2022-07-02 12:36:52 +1000

    edited custom users view

--- crowdfunding/users/models.py
+++ crowdfunding/users/models.py
@@ -2,7 +2,8 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class CustomUser(AbstractUser):
-    pass
+    profile_image = models.URLField(null = True, blank = True)
+    bio = models.TextField(max_length=200, null = True)
 
     def __str__(self):
         return self.username
--- crowdfunding/users/serializers.py
+++ crowdfunding/users/serializers.py
@@ -5,6 +5,16 @@ class CustomUserSerializer(serializers.Serializer):
     id = serializers.ReadOnlyField()
     username = serializers.CharField(max_length=200)
     email = serializers.CharField(max_length=200)
+    bio = serializers.CharField(max_length=5000)
+    profile_image = serializers.URLField()
 
     def create(self, validated_data):
         return CustomUser.objects.create(**validated_data)
+
+class CustomUserDetailSerializer(CustomUserSerializer):
+ # so you can update a single pledge
+    def update(self, instance, validated_data):
+        instance.bio = validated_data.get('bio', instance.bio)
+        instance.profile_image = validated_data.get('profile image',instance.profile_image)
+        instance.save()
+        return instance
